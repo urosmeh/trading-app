2f5df189da31f5c2d8fd2b2ea60c90d601536dae
urosmeh <uros.meh@gmail.com>
2025-02-02 13:06:10 +0100

    feat: react router install & basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 import HomePage from './pages/HomePage/HomePage.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { AppRoutes } from './constants/appRoutes.ts';
 
 function App() {
   return (
-    <div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoutes.HOME} element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
       <HomePage />
-    </div>
+    </>
   );
 }
 
