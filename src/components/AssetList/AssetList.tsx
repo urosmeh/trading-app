472208599b4b7611a6c361df957a760bbcf39edd
urosmeh <uros.meh@gmail.com>
2025-02-05 21:14:46 +0100

    feat: home page css & improvements

--- src/components/AssetList/AssetList.tsx
+++ src/components/AssetList/AssetList.tsx
@@ -2,6 +2,7 @@ import { useGetAssets } from '../../api/hooks';
 import { NavLink } from 'react-router';
 import { AppRoutes } from '../../constants/appRoutes.ts';
 import { Fragment } from 'react';
+import classes from './AssetList.module.css';
 
 type AssetListProps = {
   search: string;
@@ -12,6 +13,7 @@ const AssetList = ({ search }: AssetListProps) => {
 
   if (!search) return null;
 
+  //todo: Loading states !!!
   if (isLoading) {
     return <div>loading..</div>;
   }
@@ -20,14 +22,17 @@ const AssetList = ({ search }: AssetListProps) => {
     return <div>Theres been an error</div>;
   }
 
+  if (!data?.length) {
+    return <div>No assets found!</div>;
+  }
+
   return (
-    <div>
+    <div className={classes.container}>
       {data?.map((asset) => (
         <Fragment key={asset.id}>
           <NavLink key={asset.id} to={`${AppRoutes.ASSETS}/${asset.id}`}>
             {asset.name}
           </NavLink>{' '}
-          <br />
         </Fragment>
       ))}
     </div>
